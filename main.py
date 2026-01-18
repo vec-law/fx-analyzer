0782ee9f6be9ad33ba9fdf2d679696c632268c4d
Krzysztof Węcławik <krzysztof.weclawik@gmail.com>
2026-01-18 20:11:43 +0100

    windows (ongoing)

--- main.py
+++ main.py
@@ -9,3 +9,32 @@ def main():
 
     window.show()
     sys.exit(app.exec())
+
+__version__ = "2.0.0-alpha"
+
+import sys
+from PyQt6.QtWidgets import QApplication
+from src.user_interface import UserInterface 
+from src.utils import clear_console
+
+def main():
+    clear_console()
+    print(f"fx-analyzer v{__version__}")
+
+    app = QApplication(sys.argv)
+
+    db_config = {
+        "dbname": "fx_analyzer_db",
+        "user": "postgres",
+        "password": "1111",
+        "host": "localhost",
+        "port": 5432
+    }
+    
+    ui = UserInterface(db_config)
+    ui.show()
+    
+    sys.exit(app.exec())
+
+if __name__ == "__main__":
+    main()
